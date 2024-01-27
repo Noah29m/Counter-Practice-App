d191d4ca6a55580fd3e534b40dceb56f389d4f81
Raynafs <raynafs90@gmail.com>
2024-01-27 10:48:21 +0300

    App working

--- app/src/main/java/com/example/counterapp/ui/counter/CounterView.kt
+++ app/src/main/java/com/example/counterapp/ui/counter/CounterView.kt
@@ -1,21 +1,23 @@
 package com.example.counterapp.ui.counter
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.R
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,27 +26,36 @@ import androidx.compose.ui.unit.sp
 @Composable
     fun CounterView(){
 
-    }
+        val counter = remember {
+            mutableStateOf(0)
+        }
 
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
 
-
-@Preview
-@Composable
-fun PreviewCounter(){
-    Column (modifier = Modifier.
-    fillMaxWidth()){
+    ){
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "0",
-            fontSize = 64.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
+        Box (modifier = Modifier
+            .weight(1f),
+            contentAlignment = Alignment.Center
+
+        ){
+
+            Text(
+                text = counter.value.toString(),
+                fontSize = 120.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+
 
 
             )
+        }
         Spacer(modifier = Modifier.height(16.dp))
 
         Row (modifier = Modifier
@@ -55,23 +66,30 @@ fun PreviewCounter(){
         )
 
         {
-            Button(onClick = { /*TODO*/ },
+            Button(onClick = { counter.value +=1 },
                 modifier = Modifier.weight(1f),
                 shape = RoundedCornerShape(5.dp)
 
-                ) {
+            ) {
                 Text(text = "Add")
-                
+
             }
             Spacer(modifier = Modifier.width(8.dp))
-            Button(onClick = { /*TODO*/ },
+            Button(onClick = { counter.value -=1},
                 modifier = Modifier.weight(1f),
                 shape = RoundedCornerShape(5.dp),
                 colors = ButtonDefaults.buttonColors()
-                ) {
+            ) {
                 Text(text = "DeAdd")
-                
+
             }
+
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { counter.value =0},
+            shape = RoundedCornerShape(5.dp)) {
+
+            Text(text = "Reset")
 
         }
 
@@ -80,3 +98,11 @@ fun PreviewCounter(){
     }
 }
 
+
+
+
+
+@Preview
+@Composable
+fun PreviewCounter() {
+}
